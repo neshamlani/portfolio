e22c8c07459e969e695c236aac609bf3d51e5b25
Nesh Amlani <neshamlani1970@gmail.com>
2021-11-26 20:16:55 +0530

    feat: converted to nextjs for SEO

---
+++ src/constants/skillList.js
@@ -0,0 +1,58 @@
+import reactImage from "../assetes/reactjs.png";
+import html from "../assetes/html.png";
+import css from "../assetes/css.png";
+import firebase from "../assetes/firebase.png";
+import bootstrap from "../assetes/bootstrap.png";
+import git from "../assetes/git.png";
+import materialui from "../assetes/materialui.png";
+import redux from "../assetes/redux.png";
+import nodejs from "../assetes/nodejs.png";
+import flutter from "../assetes/flutter.png";
+import npm from "../assetes/npm.png";
+
+export const skillsList = [
+  {
+    src: reactImage,
+    title: "ReactJS",
+  },
+  {
+    src: html,
+    title: "HTML",
+  },
+  {
+    src: css,
+    title: "CSS",
+  },
+  {
+    src: firebase,
+    title: "Firebase",
+  },
+  {
+    src: bootstrap,
+    title: "BootStrap",
+  },
+  {
+    src: git,
+    title: "Git",
+  },
+  {
+    src: materialui,
+    title: "Material UI",
+  },
+  {
+    src: redux,
+    title: "Redux",
+  },
+  {
+    src: nodejs,
+    title: "Node",
+  },
+  {
+    src: flutter,
+    title: "Flutter",
+  },
+  {
+    src: npm,
+    title: "NPM",
+  },
+];
